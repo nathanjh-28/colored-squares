2277ae64b6223964946c381f1ef208d44a055a9e
nathanjh-28 <nathan@nathanjharris.com>
2022-06-23 14:23:17 -0500

    ex1 complete mapped some boxes and made state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import './style.css'
+import boxData from './boxData'
 
 function App() {
+
+  const [boxes, setBoxes] = React.useState(boxData)
+
+  const boxElements = boxes.map(box => (<div key={box.id} className="box"></div>))
+
   return (
-    <h1>React Colored Squares</h1>
+    <main>
+      {boxElements}
+    </main>
   );
 }
 
